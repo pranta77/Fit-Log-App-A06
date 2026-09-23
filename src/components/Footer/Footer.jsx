5b7446ae55086267b0b871c1b7b32807ba03f9b1
pranta77 <devpranta0@gmail.com>
2026-09-23 23:18:59 +0600

    footer sections ready

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,21 @@
-// import Image from "next/image";
-// import React from "react";
-// import logo from "@/app/assets/logo.png";
+import Image from "next/image";
+import React from "react";
+import logo from "@/app/assets/logo.png";
 
-// const Footer = () => {
-//   return (
-//     <div>
-//       <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 container mx-auto ">
-//         <aside className="grid-flow-col items-center">
-//           <Image src={logo} alt="Logo" width={30} height={30} />
-//             <p className="font-bold text-xl">FitLog</p>
-//         </aside>
-//         <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-//           <p>© 2026 FitLog — Workout Library. Train hard, log honest.</p>
-//         </nav>
-//       </footer>
-//     </div>
-//   );
-// };
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 container mx-auto ">
+        <aside className="grid-flow-col items-center">
+          <Image src={logo} alt="Logo" width={30} height={30} />
+            <p className="font-bold text-xl">FitLog</p>
+        </aside>
+        <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <p>© 2026 FitLog — Workout Library. Train hard, log honest.</p>
+        </nav>
+      </footer>
+    </div>
+  );
+};
 
-// export default Footer;
+export default Footer;
